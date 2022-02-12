380a5449b8000bd70195f31d35d7cd1498f3b35a
StephenSusanto <53929056+StephenSusanto@users.noreply.github.com>
2022-02-12 19:59:38 +0700

    coffee maker
    making coffee maker machine using dictionary

--- Day15/data.py
+++ Day15/data.py
@@ -29,3 +29,56 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+def keluarinResources():
+    water = resources["water"]
+    milk = resources["milk"]
+    coffee = resources["coffee"]
+    print(f"water: {int(water)}")
+    print(f"milk: {int(milk)}")
+    print(f"coffee: {int(coffee)}")
+
+def hitungJumlahKoin(penny, nickel, dime, quarter):
+    jumlahPenny = int(penny) * 1
+    jumlahNickel = int(nickel)*5
+    jumlahDime = int(dime)*10
+    jumlahQuarter = int(quarter)*25
+    total = (jumlahPenny + jumlahNickel + jumlahDime+jumlahQuarter) / 100
+    return total
+
+def masukanKoin(jenis):
+    a = input("How many Quarters?")
+    b = input("How many dimes?")
+    c = input("How many nickels?")
+    d = input("How many pennys?")
+    tampungHarga = MENU[jenis]["cost"]
+    totalHarga = hitungJumlahKoin(d,c,b,a) - tampungHarga
+    return totalHarga
+
+def updateResources(jenis):
+    if(jenis.lower == "espresso"):
+     aer =  resources["water"] - MENU[jenis]["ingredients"]["water"] 
+     kopi =  resources["coffee"] - MENU[jenis]["ingredients"]["coffee"] 
+     if(aer > 0 and kopi >0):
+        updateResource = {
+            "water" : aer,
+            "coffee": kopi
+        }
+        resources.update(updateResource)
+        return True
+     else:
+         return False
+    else:
+        aer =  resources["water"] - MENU[jenis]["ingredients"]["water"] 
+        susu = resources["milk"] - MENU[jenis]["ingredients"]["milk"] 
+        kopi =  resources["coffee"] - MENU[jenis]["ingredients"]["coffee"]
+        if(aer > 0 and kopi >0 and susu >0):
+            updateResource = {
+            "water" : aer,
+            "milk"  :susu,
+            "coffee": kopi
+            }
+            resources.update(updateResource)
+            return True
+        else:
+            return False
--- Day15/main.py
+++ Day15/main.py
@@ -1,6 +1,40 @@
-import data
+from data import MENU, resources, keluarinResources, masukanKoin,updateResources
 
 
-test = {"water":200}
-data.resources.update(test)
-print(data.resources)
+isOn = True
+while isOn:
+    resp = input("What would you like? espresso, latte, or cappucino:")
+    if(resp.lower() == "report"):
+        keluarinResources()
+    elif(resp.lower() == "espresso"):
+        harga = masukanKoin(resp.lower())
+        if(harga > 0):
+            if(updateResources(resp.lower()) == True):
+                print(f"Here is change {harga}")
+                print(f"Enjoy Your {resp.lower()}")
+            else:
+                print("Out of Resources of coffee")     
+        else:
+            print("Your Money is not enough")
+    elif(resp.lower() == "latte"):
+        harga = masukanKoin(resp.lower())
+        if(harga > 0):
+            if(updateResources(resp.lower()) == True):
+                print(f"Here is change {harga}")
+                print(f"Enjoy Your {resp.lower()}")
+            else:
+                print("Out of Resources of coffee") 
+        else:
+            print("Your Money is not enough")
+    elif(resp.lower() == "cappuccino"):
+        harga = masukanKoin(resp.lower())
+        if(harga > 0):
+            if(updateResources(resp.lower()) == True):
+                print(f"Here is change {harga}")
+                print(f"Enjoy Your {resp.lower()}")
+            else:
+                print("Out of Resources of coffee") 
+        else:
+            print("Your Money is not enough")
+    elif(resp.lower() == "off"):
+        isOn = False
